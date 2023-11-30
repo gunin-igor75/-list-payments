0e29c2ab55a3e475880ec108f8d0cfe3a27f6562
Igor Gunin <gunin-igor75@yandex.ru>
2023-11-30 15:45:16 +0600

    first commit

---
+++ app/src/main/java/com/github/gunin_igor75/list_payments/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.gunin_igor75.list_payments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
